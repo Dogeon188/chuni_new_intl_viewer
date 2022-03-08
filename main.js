da6352e4b715d7e8abd228cf8f6c355805d5bea7
Dogeon188 <dogcatdog188@gmail.com>
2022-03-08 14:53:04 +0800

    rating calc fix

--- main.js
+++ main.js
@@ -49,28 +49,30 @@ const getSongList = async (difficulty = Difficulty.master) => {
     return formList;
 }
 
-const ratingCalc = (score, songRating) => {
+const ratingCalc = (score, chartConst) => {
     let offset = 0;
 
     if (score >= 1009000) {
         offset = 2.15;
     } else if (score >= 1007500) {
-        offset = 2 + (score - 1007500) * 5 / 50000;
+        offset = 2 + (score - 1007500) / 1500 * 0.15;
     } else if (score >= 1005000) {
-        offset = 1.5 + (score - 1005000) * 10 / 50000;
+        offset = 1.5 + (score - 1005000) / 2500 * 0.5;
     } else if (score >= 1000000) {
-        offset = 1 + (score - 1000000) * 5 / 50000;
+        offset = 1 + (score - 1000000) / 5000 * 0.5;
     } else if (score >= 975000) {
-        offset = (score - 975000) * 2 / 50000;
-    } else if (score >= 950000) {
-        offset = -1.5 + (score - 950000) * 3 / 50000;
+        offset = (score - 975000) / 25000;
     } else if (score >= 925000) {
-        offset = -3 + (score - 925000) * 3 / 50000;
-    } else {
-        offset = -5 + (score - 900000) * 4 / 50000;
-    }
+        offset = -3 + (score - 925000) / 50000 * 3;
+    } else if (score >= 900000) {
+        offset = -5 + (score - 900000) / 25000 * 2;
+    } else if (score >= 800000) {
+        return (chartConst - 5) / 2 * (1 + (score - 800000) / 100000);
+    } else if (score >= 500000) {
+        return (chartConst - 5) / 2 * (score - 500000) / 300000;
+    } else return 0;
 
-    return strToNum((songRating + offset).toFixed(2));
+    return chartConst + offset;
 }
 
 const recordFetch = async () => {
