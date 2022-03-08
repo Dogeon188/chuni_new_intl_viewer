e7a26f699f175d3f6bfe77846dc129d87577e1a6
Dogeon188 <dogcatdog188@gmail.com>
2022-03-08 12:49:18 +0800

    test

--- main.js
+++ main.js
@@ -106,14 +106,14 @@ const recordFetch = async () => {
 const main = async () => {
     if (window.location.hostname !== "chunithm-net-eng.com") {
         alert("[chuni_intl_viewer] This tools could only be used under chunithm-net international.");
-        // window.location.href = "https://chunithm-net-eng.com/";
+        window.location.href = "https://chunithm-net-eng.com/";
         return;
     }
 
     if (!getCookie("_t")) {
         alert("[chuni-intl-viewer] Token not found. Please login first.");
         msgEl.innerText = document.cookie;
-        window.location.href = "https://chunithm-net-eng.com/";
+        // window.location.href = "https://chunithm-net-eng.com/";
         return;
     }
 
