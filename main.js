292442366df0e46089f8c1594381969103aa877f
Dogeon188 <dogcatdog188@gmail.com>
2022-03-08 12:46:25 +0800

    test

--- main.js
+++ main.js
@@ -106,7 +106,7 @@ const recordFetch = async () => {
 const main = async () => {
     if (window.location.hostname !== "chunithm-net-eng.com") {
         alert("[chuni_intl_viewer] This tools could only be used under chunithm-net international.");
-        window.location.href = "https://chunithm-net-eng.com/";
+        // window.location.href = "https://chunithm-net-eng.com/";
         return;
     }
 
